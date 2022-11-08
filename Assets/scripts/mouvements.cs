12948f4ffa8f3855431cb5657c4556bea6caf9e4
Rubylium <19718604+Rubylium@users.noreply.github.com>
2022-11-08 16:38:05 +0100

    init

---
+++ Assets/scripts/mouvements.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class mouvements : MonoBehaviour
+{
+    [SerializeField] private float playerSpeed = 5.0f;
+    [SerializeField] private float jumpPower = 5.0f;
+    
+    private Rigidbody2D _playerRigidbody;
+    private void Start()
+    {
+        _playerRigidbody = GetComponent<Rigidbody2D>();
+        if (_playerRigidbody == null)
+        {
+            Debug.LogError("Player is missing a Rigidbody2D component");
+        }
+    }
+    private void Update()
+    {
+        MovePlayer();
+
+        if (Input.GetKey(KeyCode.Space))
+            Jump();
+    }
+    private void MovePlayer()
+    {
+        var horizontalInput = Input.GetAxisRaw("Horizontal");
+        _playerRigidbody.velocity = new Vector2(horizontalInput * playerSpeed, _playerRigidbody.velocity.y);
+    }
+    private void Jump() => _playerRigidbody.velocity = new Vector2( 0, jumpPower);
+}
